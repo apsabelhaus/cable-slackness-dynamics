c2c8c73ae0e01dc25a3b64ebeb233bac479b1b9a
Andrew Sabelhaus <apsabelhaus@berkeley.edu>
2018-07-19 17:18:20 -0700

    Worked through the proof for output strict passivity of x L(x). May still have errors.

--- logistic_slope_max.m
+++ logistic_slope_max.m
@@ -1,8 +1,11 @@
 % symbolically calculating the maximum slope of the logistic function. 
+% Andrew P. Sabelhaus, 2019
 
 clear all;
 close all;
 clc;
+
+%% for just the logistic function
 
 % declare the one symbolic variable
 
@@ -16,7 +19,6 @@ t = linspace(-10, 10, 1000);
 L_subbed = eval( subs(L, x, t) );
 plot(t, L_subbed);
 title('L(x)');
-
 
 %  its slope is
 dLdx = L * (1 - L)
@@ -53,6 +55,79 @@ eval(max_slope)
 
 % somehow this equals a quarter. So, the maximum slope of L(x) is is 1/4.
 % ...write that out in my notebook.
+
+%% Now for x L(x), what we're really interested in.
+
+disp('Now for the cable model itself:');
+
+% set up the logistically-smoothed cable function
+xLx = x / (1 + exp(-x) )
+
+% plot of the base function itself
+xLx_subbed = eval( subs(xLx, x, t) );
+figure;
+plot(t, xLx_subbed);
+title('xL(x)');
+
+% its slope is
+d_xLx_dx = x * L * (1 - L) + L
+pretty( simplify(d_xLx_dx) )
+
+% plot this one too
+d_xLx_dx_subbed = eval( subs(d_xLx_dx, x, t) );
+figure;
+plot(t, d_xLx_dx_subbed);
+title('d xLx dx');
+
+% Interesting. It looks visually like 
+% lim(x -> -inf) = 0, 
+% min @ some -x and is less than 1,
+% value @ 0 = 0.5,
+% max @ some +x and is greater than 1,
+% lim(x -> +inf) = 1.
+
+% So, let's do like before, and find the mimima and maxima.
+d2_xLx_dx2 = simplify( diff( d_xLx_dx ) )
+pretty( d2_xLx_dx2 )
+
+% plot it:
+d2_xLx_dx2_subbed = eval( subs( d2_xLx_dx2, x, t ) );
+figure;
+plot(t, d2_xLx_dx2_subbed );
+title('d2 xLx dx2');
+
+% this function definitely looks like it has a zero. Let's find it.
+solve( d2_xLx_dx2 == 0 )
+
+% Something around 2.4 (doesn't seem to have an exact answer?)
+% Substituting x=2.4 back in to d_xLx_dx ~= 1.1. So, there's probably a
+% constant bound here (hooray! that's what we hoped for), but it doesn't
+% seem to have a nice value. But maybe the constant itself just can't be
+% dealt with by MATLAB's symbolic solver, so see my notebook for an attempt
+% to solve d2_xLx_dx2 == 0 by hand.
+
+%% Instead, let's get a bound.
+
+% From the written notes, we need to bound the function
+bound1 = x / (exp(x) + 1)
+
+% So let's find the maximum of its derivative, which is in part the
+% following (note that I've removed all the other terms that don't
+% contribute to the zero (the denominator, for example.) see notes.
+% (remember we're just finding the loc of the max right here)
+d_bound1_dx = exp(x) + 1 - x * exp(x)
+
+% Solving is
+bound1_max_location = solve( d_bound1_dx == 0)
+
+% Then the bound is
+bound1_max = subs( bound1, x, bound1_max_location)
+eval(bound1_max)
+
+% which then is \alpha = 1 + bound1_max proof, so bound overall is
+% ...wait, do I do 1 + 1 + 0.278 or just 1 + 0.278??? maybe mistake.
+
+
 
 
 
